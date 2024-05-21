6e4ddec8b31c942c18825292615371f09c1442ec
RUSTEMATOR <samoilenkorustem@gmail.com>
2024-05-21 17:21:06 +0300

    added pre-vip

--- Data/account_info.py
+++ Data/account_info.py
@@ -5,6 +5,7 @@ credentials = {
     "3_dep": {"username": "nikita3@gmail.com", "password": "KingBillyCasino1@"},
     "4_dep": {"username": "nikita4@gmail.com", "password": "KingBillyCasino1@"},
     "5_dep": {"username": "nikita5@gmail.com", "password": "KingBillyCasino1@"},
+    "previp": {"username": "previp_test@kingbilly.xyz", "password": "193786Az()"},
     "vip": {"username": "vip@kingbilly.xyz", "password": "LOL12lol!"}
 }
 
